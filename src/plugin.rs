799a226992b1b872a6db7fbf23bfea106bad8879
miampf <miampf@proton.me>
2024-07-16 21:50:44 +0200

    added forgotten eq

--- src/plugin.rs
+++ src/plugin.rs
@@ -3,14 +3,14 @@ use std::collections::HashMap;
 use extism_convert::{FromBytes, Msgpack, ToBytes};
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, ToBytes, FromBytes, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, ToBytes, FromBytes, Serialize, Deserialize)]
 #[encoding(Msgpack)]
 /// Input of a plugin defined function.
 pub struct Input<T> {
     pub config: HashMap<String, String>,
     pub data: T,
 }
-#[derive(Clone, Debug, ToBytes, FromBytes, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, ToBytes, FromBytes, Serialize, Deserialize)]
 #[encoding(Msgpack)]
 /// Output of a plugin defined function.
 pub struct Output<T> {
